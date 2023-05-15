7e70295425faef79ddbf77eb5185b29bf3ad2d49
Joydip <joydip.mondal@gmail.com>
2023-05-15 17:54:39 +0530

    day-1 final upload

--- codes/day-1/calculator-app/source/index.ts
+++ codes/day-1/calculator-app/source/index.ts
@@ -10,5 +10,18 @@ const calculate = () => {
     const result = add(first, second)
 
     const resultTxtBox = <HTMLInputElement>document.getElementById('txtResult')
-    resultTxtBox.innerText = result.toString()
+    resultTxtBox.value = result.toString()
+    console.log(result)
 }
+
+document
+    .getElementById('btnCalulate')
+    ?.addEventListener('click', calculate)
+
+// window.addEventListener(
+//     'DOMContentLoaded',
+//     function () {
+//         document
+//             .getElementById('btnCalulate')
+//             ?.addEventListener('click', calculate)
+//     })
