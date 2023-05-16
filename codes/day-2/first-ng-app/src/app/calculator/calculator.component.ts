0ab16fade109bfc067d8e3030c1c9f2bcbd92311
Joydip <joydip.mondal@gmail.com>
2023-05-16 17:37:05 +0530

    day-2 final upload

--- codes/day-2/first-ng-app/src/app/calculator/calculator.component.ts
+++ codes/day-2/first-ng-app/src/app/calculator/calculator.component.ts
@@ -11,4 +11,17 @@ export class CalculatorComponent {
   // changeMessage(msg: string) {
   //   this.message = msg
   // }
+
+  first = 0
+  second = 0
+  result = 0
+
+  // calculate(f: number, s: number) {
+  //   this.first = f
+  //   this.second = s
+  //   this.result = this.first + this.second
+  // }
+  calculate() {
+    this.result = Number(this.first) + (+this.second)
+  }
 }
