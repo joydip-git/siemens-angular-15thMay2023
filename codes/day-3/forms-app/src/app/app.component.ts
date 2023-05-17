c6188c30b163d53e79e23d4c4e7c306a77fabcd0
Joydip <joydip.mondal@gmail.com>
2023-05-17 11:14:00 +0530

    form and validation upload

--- codes/day-3/forms-app/src/app/app.component.ts
+++ codes/day-3/forms-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-app';
+  // first = new FormControl(0)
+  // second = new FormControl(0)
+  // calcForm = new FormGroup({
+  //   firstCtrl: this.first,
+  //   secondCtrl: this.second
+  // })
+  calcForm = new FormGroup({
+    first: new FormControl('', [Validators.required, Validators.min(1)]),
+    second: new FormControl('', [Validators.required, Validators.min(1)])
+  })
+  result = 0
+
+  get first() {
+    return this.calcForm.get('first')
+  }
+  get second() {
+    return this.calcForm.controls['second']
+  }
+  calculate() {
+    //this.result = this.first?.value + this.second?.value
+    console.log(this.calcForm)
+    //this.calcForm.controls['first']?.value
+    const firstNum = this.first?.value
+    const secondNum = this.second?.value
+    if (firstNum && secondNum) {
+      this.result = (+firstNum) + (+secondNum)
+    }
+  }
 }
